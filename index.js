7b18c346d7acd5eaaed18eaacc062362ba6c79f9
pillai.ashwini@gmail.com <pillai.ashwini@gmail.com>
2018-07-19 08:28:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var i=0;
   while(i < facts.length) {
-    johnLennon[i] = [`${johnLennon[i]}!!!`];
+    facts[i] = [`${facts[i]}!!!`];
     i++;
   }
   return newArray;
