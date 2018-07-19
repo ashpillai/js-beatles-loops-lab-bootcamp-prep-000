b2a4140041c2e9eb778fd0c38e2cf0fb4fa38432
pillai.ashwini@gmail.com <pillai.ashwini@gmail.com>
2018-07-19 07:20:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for(var i=0; i< musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(johnLennon) {
+  while(johnLennon.length>0) {
+    johnLennon[i] = [`${johnLennon[i]}!`];
+    i++
+  }
+}
